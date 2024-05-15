d82c6886f68c83724a38bae12c5557134f2dbbd0
BerkantGC <berkant103x@hotmail.com>
2024-05-16 00:39:54 +0200

    skills updated

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import styles from '../styles/Experience.css';
+import styles from "../styles/Experience.css";
+import Colors from "../Colors";
 
 const Experience = () => {
   return (
-    <section id="experience" className={styles.experience}>
-      <h2>Experience</h2>
-      <div>
-        <h3>YU MOBILE APP CLUB</h3>
-        <p>FULL STACK DEVELOPER</p>
-        <p>Aug 2022 - Dec 2023</p>
-        <p>I served as a Full Stack Developer at the YU Mobile Application Club...</p>
-      </div>
-      <div>
-        <h3>IBOST</h3>
-        <p>FRONTEND WEB DEVELOPER</p>
-        <p>May 2023 - June 2023</p>
-        <p>I have undertaken the development and design of a website for the emerging technology company, iBost...</p>
-      </div>
-      <div>
-        <h3>GAMESATIŞ</h3>
-        <p>SOFTWARE ENGINEER INTERN</p>
-        <p>Jun 2022 - Sep 2022</p>
-        <p>During the 4 months of internship, I have significantly enhanced my skills in frontend and backend development...</p>
-      </div>
-      <div>
-        <h3>IEEE YAŞAR UNIVERSITY BRANCH</h3>
-        <p>PRESIDENT & FULL-STACK DEVELOPER</p>
-        <p>Jan 2024 - Now</p>
-        <p>IEEE (Engineering) Community, one of the student communities of Yaşar University...</p>
+    <section id="experience">
+      <h2>Work Experience</h2>
+      <div className='experience-content'>
+        <div style={{backgroundColor: Colors.sunlitOrange, borderColor: Colors.lines}}>
+          <h3>IEEE YAŞAR UNIVERSITY BRANCH</h3>
+          <p>PRESIDENT & FULL-STACK DEVELOPER<br/><span>Jan 2024 - Now</span></p>
+          <p>IEEE (Engineering) Community, one of the student communities of Yaşar University...</p>
+          <div>See details</div>
+        </div>
+
+        <div style={{backgroundColor: Colors.sunlitOrange,borderColor: Colors.lines}}>
+          <h3>YU MOBILE APP CLUB</h3>
+          <p>FULL STACK DEVELOPER<br/><span>Aug 2022 - Dec 2023</span></p>
+          <p>I served as a Full Stack Developer at the YU Mobile Application Club...</p>
+          <div>See details</div>
+        </div>
+
+        <div style={{backgroundColor: Colors.sunlitOrange, borderColor: Colors.lines}}>
+          <h3>IBOST</h3>
+          <p>FRONTEND WEB DEVELOPER<br/><span>May 2023 - June 2023</span></p>
+          <p>I have undertaken the development and design of a website for the emerging technology company, iBost...</p>
+          <div>See details</div>
+        </div>
+
+        <div style={{backgroundColor: Colors.sunlitOrange, borderColor: Colors.lines}}>
+          <h3>GAMESATIŞ</h3>
+          <p>SOFTWARE DEVELOPER INTERN<br/><span>Jun 2022 - Sep 2022</span></p>
+          <p>During the 4 months of internship, I have significantly enhanced my skills in frontend and backend development...</p>
+          <div>See details</div>
+        </div>
       </div>
     </section>
   );
