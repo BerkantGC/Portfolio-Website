6449271831874751fb557bb42c7b077f5ae91434
BerkantGC <berkant103x@hotmail.com>
2024-05-16 11:05:49 +0200

    projects added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,10 +12,10 @@ const Projects = () => {
     <section id="projects" className="projects">
       <h2>Projects</h2>
       <div className='projects-details'>
-        <div style={{backgroundImage: `url(${YaşarForumImage})`}}>
+        <div href="https://github.com/BerkantGC/YasarForum" style={{backgroundImage: `url(${YaşarForumImage})`}}>
           <div>YAŞAR FORUM<p>I developed this project for Yaşar University students to share their ideas, class
             notes and announcements freely. The project is developed by using React Native,
-            Spring Boot and MySQL.</p>
+            Spring Boot and MySQL.<br/><a target='_blank' href='https://github.com/BerkantGC/YasarForum'>Link</a></p>
           </div>
         </div>
 
@@ -23,14 +23,14 @@ const Projects = () => {
           <div>FILE ENCRYPTION AND SIGNING SOFTWARE<p>
             A data (file) encryption & signing software with English user interface, using Public
             Key Infrastructure (PKI). It is a web-based application. Hash (SHA-512), symmetric
-            encryption(AES) and asymmetric encryption(RSA) were all used in project</p>
+            encryption(AES) and asymmetric encryption(RSA) were all used in project<br/><a target='_blank' href='https://github.com/BerkantGC/File-Encryption-Signing-Software'>Link</a></p>
             </div>
         </div>
 
         <div style={{backgroundImage: `url(${SportfriendImage})`}}>
           <div>SPORTFRIEND APP<p>This project is developed for SE 2222 Database Systems:
           The Sportfriend App is a platform designed for users to share their sports activities, connect with friends, and rate different sports. 
-          It's built using Spring for the backend, React.js for the frontend, and MySQL for the database.</p></div>
+          It's built using Spring for the backend, React.js for the frontend, and MySQL for the database.<br/><a target='_blank' href='https://github.com/BerkantGC/Sportfriend-App'>Link</a></p></div>
         </div>
 
         <div style={{backgroundImage: `url(${MugTreeImage})`}}>
@@ -38,21 +38,21 @@ const Projects = () => {
             <p>Website which is developed for Mobile App Club of Yaşar University:
             The clone of tree websites which are directing the user to related links. Also
             activities and games can be seen and downloadable. Firebase and React.JS are
-            used.</p>
+            used.<br/><a href='https://yumug.netlify.app/' target='_blank'>Link</a></p>
           </div>
         </div>
 
         <div style={{backgroundImage: `url(${MovieAppImage})`}}>
           <div>MOVIE APP<p>The Movie App is a mobile-friendly project using React Native and Firebase. Users
           can browse movies, view details such as ratings and reviews, discover and explore
-          information about movies.</p></div>
+          information about movies.<br/><a target='_blank' href='https://github.com/BerkantGC/Movie-App'>Link</a></p></div>
         </div>
 
         <div style={{backgroundImage: `url(${FindMazeExitImage})`}}>
           <div>FIND EXIT OF MAZE<p>Developed by using C language and data. Game utilizes fundamental data
           structures such as arrays, linked lists, or graphs to represent the maze and
           implement algorithms for maze traversal, such as depth-first search (DFS) or
-          breadth-first search (BFS).</p></div>
+          breadth-first search (BFS).<br/><a target='_blank' href='https://github.com/BerkantGC/Find-Exit-of-Maze'>Link</a></p></div>
         </div>
       </div>
     </section>
